d871e99dffa54a506aa79c55351d5d4c0de64f93
Michael Wang <michaelw0395@gmail.com>
2020-01-04 00:47:43 +1100

    additional review feature added, users can add their own reviews to restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,32 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.where(restaurant_id: params[:restaurant_id])
     end
+    
+    def new
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(strong_review_params)
+        @restaurant = Restaurant.find(params[:restaurant_id])
+
+        # Connecting review to restaurant
+        @review.restaurant = @restaurant
+        # Connecting review to user
+        @review.user = current_user
+        
+        if @review.save
+            # if true, @review.save will save the instance
+            redirect_to restaurant_reviews_path(@restaurant)
+        else
+            render :new
+        end
+    end
+
+    private
+
+  def strong_review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  # validations
+  validates :comment, presence: true
+  validates :rating, inclusion: {in: [1,2,3,4,5]}
 end
