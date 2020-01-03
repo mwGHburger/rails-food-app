ff80a4369593128a3b4effcd64b718559c62f982
Michael Wang <michaelw0395@gmail.com>
2020-01-03 13:34:48 +1100

    Added minimal styling to Restaurant cards and layout, added seed data with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating Restaurants"
+    10.times do
+        restaurant = Restaurant.new(
+            name: Faker::Restaurant.name,
+            address: Faker::Address.street_address,
+            description: Faker::Restaurant.description,
+            cuisine: Faker::Nation.nationality,
+            rating: rand(6)
+        )
+        restaurant.save
+    end
+puts "Finished Creating Restaurants"
